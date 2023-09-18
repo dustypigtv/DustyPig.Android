9233a49717f921a476928debb702481790788b37
JD <dev@dustypig.tv>
2023-09-18 11:33:33 -0600

    Remove unused code

--- app/src/main/java/tv/dustypig/dustypig/ui/composables/DpPx.kt
+++
@@ -1,8 +0,0 @@
-package tv.dustypig.dustypig.ui.composables
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
-
-@Composable
-fun Dp.toPXInt() = with(LocalDensity.current) { this@toPXInt.roundToPx() }
