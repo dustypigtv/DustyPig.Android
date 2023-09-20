4d6e2d81e7f5211bfd71a1b5aeb30310621b591d
JD <dev@dustypig.tv>
2023-09-20 15:59:18 -0600

    Added new settings

--- app/src/main/java/tv/dustypig/dustypig/global_managers/SettingsManager.kt
+++ app/src/main/java/tv/dustypig/dustypig/global_managers/SettingsManager.kt
@@ -10,60 +10,88 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
-class SettingsManager @Inject constructor(@ApplicationContext private val context: Context) {
+class SettingsManager @Inject constructor (
+    @ApplicationContext private val context: Context
+) {
 
-    private val AUTH_TOKEN_KEY = stringPreferencesKey("auth_token")
-    private val PROFILE_ID_KEY = intPreferencesKey("profile_id")
-    private val IS_MAIN_PROFILE_KEY = booleanPreferencesKey("is_main_profile")
-    private val STORE_DOWNLOADS_EXTERNALLY_KEY = booleanPreferencesKey("store_downloads_externally")
-    private val DOWNLOAD_OVER_CELLULAR_KEY = booleanPreferencesKey("download_over_cellular")
-    private val SKIP_INTROS = booleanPreferencesKey("skip_intros")
-    private val SKIP_CREDITS = booleanPreferencesKey("skip_credits")
+    companion object {
+        private const val STORE_DOWNLOADS_EXTERNALLY_KEY = "store_downloads_externally"
+        private const val DOWNLOAD_OVER_CELLULAR_KEY = "download_over_cellular"
+        private const val SKIP_INTROS_KEY = "skip_intros"
+        private const val SKIP_CREDITS_KEY = "skip_credits"
+        private const val RECEIVE_NOTIFICATIONS_KEY = "receive_notifications"
+    }
+
+
+    private val authTokenPreferencesKey = stringPreferencesKey("auth_token")
+    private val profileIdPreferencesKey = intPreferencesKey("profile_id")
+    private val isMainProfilePreferenesKey = booleanPreferencesKey("is_main_profile")
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 
+    // ***** Global Settings *****
+
     fun loadToken(): Flow<String> {
         return context.dataStore.data.map { preferences ->
-            preferences[AUTH_TOKEN_KEY] ?: ""
+            preferences[authTokenPreferencesKey] ?: ""
         }
     }
 
     suspend fun saveToken(token: String) {
         context.dataStore.edit { preferences ->
-            preferences[AUTH_TOKEN_KEY] = token
+            preferences[authTokenPreferencesKey] = token
         }
     }
 
-
-
-
     fun loadProfileId() : Flow<Int>{
         return context.dataStore.data.map { preferences ->
-            preferences[PROFILE_ID_KEY] ?: 0
+            preferences[profileIdPreferencesKey] ?: 0
         }
     }
 
     suspend fun saveProfileId(profileId: Int){
         context.dataStore.edit { preferences ->
-            preferences[PROFILE_ID_KEY] = profileId
+            preferences[profileIdPreferencesKey] = profileId
+        }
+    }
+
+    fun loadIsMainProfile(): Flow<Boolean> {
+        return context.dataStore.data.map { preferences ->
+            preferences[isMainProfilePreferenesKey] ?: false
+        }
+    }
+
+    suspend fun saveIsMainProfile(isMainProfile: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[isMainProfilePreferenesKey] = isMainProfile
         }
     }
 
 
 
 
+
+    // ***** Profile Specific Settings Below Here *****
+
+
+    private suspend fun profileBooleanPreferencesKey(key: String): Preferences.Key<Boolean> {
+        val profileId = loadProfileId().first()
+        return booleanPreferencesKey("${profileId}_${key}")
+    }
+
     fun loadStoreDownloadsExternally(): Flow<Boolean> {
 
         return context.dataStore.data.map { preferences ->
             if(android.os.Environment.getExternalStorageState() == android.os.Environment.MEDIA_MOUNTED) {
-                preferences[STORE_DOWNLOADS_EXTERNALLY_KEY] ?: true
+                preferences[profileBooleanPreferencesKey(STORE_DOWNLOADS_EXTERNALLY_KEY)] ?: true
             }
             else {
                 false
@@ -73,36 +101,56 @@ class SettingsManager @Inject constructor(@ApplicationContext private val contex
 
     suspend fun saveStoreDownloadsExternally(value: Boolean) {
         context.dataStore.edit { preferences ->
-            preferences[STORE_DOWNLOADS_EXTERNALLY_KEY] = value
+            preferences[profileBooleanPreferencesKey(STORE_DOWNLOADS_EXTERNALLY_KEY)] = value
         }
     }
-
-
-
-
-    fun loadIsMainProfile(): Flow<Boolean> {
-        return context.dataStore.data.map { preferences ->
-            preferences[IS_MAIN_PROFILE_KEY] ?: false
-        }
-    }
-
-    suspend fun saveIsMainProfile(isMainProfile: Boolean) {
-        context.dataStore.edit { preferences ->
-            preferences[IS_MAIN_PROFILE_KEY] = isMainProfile
-        }
-    }
-
-
 
     fun loadDownloadOverCellular() : Flow<Boolean> {
         return context.dataStore.data.map { preferences ->
-            preferences[DOWNLOAD_OVER_CELLULAR_KEY] ?: false
+            preferences[profileBooleanPreferencesKey(DOWNLOAD_OVER_CELLULAR_KEY)] ?: false
         }
     }
 
     suspend fun saveDownloadOverCellular(value: Boolean) {
         context.dataStore.edit { preferences ->
-            preferences[DOWNLOAD_OVER_CELLULAR_KEY] = value
+            preferences[profileBooleanPreferencesKey(DOWNLOAD_OVER_CELLULAR_KEY)] = value
+        }
+    }
+
+    fun loadSkipIntros(): Flow<Boolean> {
+        return context.dataStore.data.map { preferences ->
+            preferences[profileBooleanPreferencesKey(SKIP_INTROS_KEY)] ?: false
+        }
+    }
+
+    suspend fun saveSkipIntros(value: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[profileBooleanPreferencesKey(SKIP_INTROS_KEY)] = value
+        }
+    }
+
+
+    fun loadSkipCredits(): Flow<Boolean> {
+        return context.dataStore.data.map { preferences ->
+            preferences[profileBooleanPreferencesKey(SKIP_CREDITS_KEY)] ?: false
+        }
+    }
+
+    suspend fun saveSkipCredits(value: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[profileBooleanPreferencesKey(SKIP_CREDITS_KEY)] = value
+        }
+    }
+
+    fun loadReceiveNotifications(): Flow<Boolean> {
+        return context.dataStore.data.map { preferences ->
+            preferences[profileBooleanPreferencesKey(RECEIVE_NOTIFICATIONS_KEY)] ?: true
+        }
+    }
+
+    suspend fun saveReceiveNotifications(value: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[profileBooleanPreferencesKey(RECEIVE_NOTIFICATIONS_KEY)] = value
         }
     }
 
